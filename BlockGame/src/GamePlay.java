084768b5af10a8f846603c729760c2e90e969b38
Allison Snipes <ajsnipes@usfca.edu>
2020-04-13 17:33:32 -0700

    adding the listeners and cleaning up code

--- BlockGame/src/GamePlay.java
+++ BlockGame/src/GamePlay.java
@@ -1,4 +1,36 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JPanel;
 
-public class GamePlay {
+public class GamePlay extends JPanel implements KeyListener, ActionListener {
+	//these are all of the implemented methods for the action and key listeners
+	
+	//actionlistener that moves the ball around
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//keylistener to move the slider
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- BlockGame/src/Main.java
+++ BlockGame/src/Main.java
@@ -6,12 +6,15 @@ public class Main {
 		JFrame obj = new JFrame();
 		obj.setBounds(10, 10, 700, 700); //sets the size of the window
 		obj.setTitle("Block Game"); //title of the window
-		obj.setResizable(false); //makes the window fixed not resizable
-		obj.setVisible(true); //makes the window visable
+		obj.setResizable(false); //makes the window fixed not re-sizable
+		obj.setVisible(true); //makes the window visible
 		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exits game on close
 		
-		GamePlay gamePlay = new GamePlay(); //we needed an object of GamePlay so that it is called within the jframe
-		
+		GamePlay gamePlay = new GamePlay(); //we needed an object of GamePlay class so that it is called within the main class
+		obj.add(gamePlay);
+	
+	
+	
 	}
 
 
