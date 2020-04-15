b95a43fb97c1f145f3c0f089565c0b0b83269287
Allison Snipes <ajsnipes@usfca.edu>
2020-04-14 20:45:51 -0700

    drawing out the game

--- BlockGame/src/GamePlay.java
+++ BlockGame/src/GamePlay.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,6 +15,7 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 	private Timer timer; //timer for setting a goal for the level
 	private int delay = 8; //speed for the timer
 	
+	private int playerX = 310; // give the player some sort of value
 	private int xSliderPosition = 310; //the starting position of the slider
 	private int xBallPosition = 120; //the starting x-axis position of the ball
 	private int yBallPosition = 350; //the starting x-axis position of the ball
@@ -29,7 +31,26 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
 		timer.start();
 		
 	}
-	public void paint(Graphics g ); //draws the slider ball and different objects in the game
+	
+	public void paint(Graphics g) {//draws the slider ball and different objects in the game
+		//drawing background
+		g.setColor(Color.black);
+		g.fillRect(1, 1, 692, 592);
+	
+		//drawing borders
+		g.setColor(Color.yellow);
+		g.fillRect(0, 0, 3, 592);
+		g.fillRect(0, 0, 692, 3);
+		g.fillRect(691, 0, 3, 592); //if the ball goes all the way down it should end the game
+		
+		//drawing the paddle
+		g.setColor(Color.green);
+		g.fillRect(playerX, 550, 100, 8);
+		
+		//drawing the ball
+		g.setColor(Color.yellow);
+		g.fillRect(xBallPosition, yBallPosition, 20, 20);
+	} 
 	
 	
 	
